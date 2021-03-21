0986b0d4539b180c5f1f4d12dae4a4037664a325
3neutronstar <3neutronstar@naver.com>
2021-03-21 17:48:42 +0900

    remove the gui option for clearing json setting, and add the structure

--- configs.py
+++ configs.py
@@ -1,27 +1,30 @@
 Vehicles = list()
 
 EXP_CONFIGS = {
-    'num_cars' : 1800,
-    'vehicle_info' : Vehicles,
-    'file_name' : None,
-    'network' : None,
-    'max_step' : 3600,
-    'current_path' : 'abspath',
-    'gui' : False,
+    'num_cars': 1800,
+    'vehicle_info': Vehicles,
+    'file_name': None,
+    'network': None,
+    'max_step': 3600,
+    'current_path': 'abspath',
+    'gui': False,
 
-    #NET_CONFIGS
-    'NET_CONFIGS' : {
-        'grid_num' : 0,
-        'lane_num' : 0, #lane 개수
+    # NET_CONFIGS
+    # CALL FROM the Network dir
+    'NET_CONFIGS': {
+        'grid_num': 0,
+        'lane_num': 0,  # lane 개수
     },
 
-    #ENV_CONFIGS
-    'ENV_CONFIGS' : {
+    # ENV_CONFIGS4
+    # CALL FROM the Env dir
+    'ENV_CONFIGS': {
 
     },
 
-    #AGENT_CONFIGS
-    'AGENT_CONFIGS' : {
-        'algorithm' : None
+    # AGENT_CONFIGS
+    # CALL FROM the Agent dir
+    'AGENT_CONFIGS': {
+        'algorithm': None
     }
 }
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 import traci
+import time
 from configs import EXP_CONFIGS
 from sumolib import checkBinary
 
@@ -16,36 +17,45 @@ def parse_args(args):
                         'train', 'simulate', 'test'])
 
     # 추가 옵션
-    parser.add_argument('--network', type=str)
-    parser.add_argument('--display', type=bool, default=False)
-    parser.add_argument('--algorithm', type=str, default='algorithm')
+
+    # choose road network
+    parser.add_argument('--net', type=str)
+    # display the monitor
+    parser.add_argument('--disp', type=bool, default=False)
+    # algorithm decision
+    parser.add_argument('--alg', type=str, default='algorithm')
     return parser.parse_known_args(args)[0]
 
 
-def test(flags, configs, sumoConfig):
-    sumoCmd = [configs['gui'], "-c", sumoConfig]
+def test(flags, configs, sumoBinary, sumoConfig):
+    sumoCmd = [sumoBinary, "-c", sumoConfig]
     # 알고리즘 평가
-
-
-def train(flags, configs, sumoConfig):
-    sumoCmd = [configs['gui'], "-c", sumoConfig]
-    # config 값 세팅하고, 지정된 알고리즘으로 트레이닝
     traci.start(sumoCmd)
     step = 0
     while step < configs['max_step']:
         traci.simulationStep()
         step += 1
 
+
+def train(flags, configs, sumoBinary, sumoConfig):
+    sumoCmd = [sumoBinary, "-c", sumoConfig]
+    # config 값 세팅하고, 지정된 알고리즘으로 트레이닝
+    traci.start(sumoCmd)
+    step = 0
+    while step < configs['max_step']:
+        traci.simulationStep()  # agent.step안에 들어가야함
+        step += 1
+
     traci.close()
 
 
-def simulate(flags, configs, sumoConfig):
-    sumoCmd = [configs['gui'], "-c", sumoConfig]
+def simulate(flags, configs, sumoBinary, sumoConfig):
+    sumoCmd = [sumoBinary, "-c", sumoConfig]
     traci.start(sumoCmd)
     traci.simulation.subscribe()
     step = 0
     while step < configs['max_step']:
-        traci.simulationStep()
+        traci.simulationStep()  # agent.step안에 들어가야함
         step += 1
 
     traci.close()
@@ -53,18 +63,20 @@ def simulate(flags, configs, sumoConfig):
 
 def main(args):
     flags = parse_args(args)
+    running_path = os.path.dirname(os.path.abspath(__file__))
 
     # config 파일에서 데이터 가져오기
-    configs = EXP_CONFIGS
-    configs['mode'] = flags.mode
-    configs['current_path'] = os.path.dirname(os.path.abspath(__file__))
+    if flags.mode == 'test':
+        configs = load_params(running_path)
+    else:  # simulate or train
+        configs = EXP_CONFIGS
 
     # 네트워크 호출
-    configs['network'] = "네트워크 이름"
+    configs['network'] = flags.network.lower()
 
     # 어떤 네트워크인지 체크
     if configs['network'] == "네트워크 이름":
-        configs['NET_CONFIGS']["gird_num"] = 3
+        configs['NET_CONFIGS']["grid_num"] = 3
         # 추가..
         # #net 생성
 
@@ -80,23 +92,22 @@ def main(args):
         sumoBinary = checkBinary('sumo-gui')
     else:
         sumoBinary = checkBinary('sumo')
-    configs['gui'] = sumoBinary
 
     # 모드 결정 및 실행
-    if configs['mode'] == 'train':
+    if flags.mode.lower() == 'train':
         sumoConfig = os.path.join(
-            configs['current_path'], 'training_data', '_train.sumocfg')  # 중간 파일 경로 추가
-        train(flags, configs, sumoConfig)
+            running_path, 'training_data', '_train.sumocfg')  # 중간 파일 경로 추가
+        train(flags, configs, sumoBinary, sumoConfig)
 
-    elif configs['mode'] == 'test':
+    elif flags.mode.lower() == 'test':
         sumoConfig = os.path.join(
-            configs['current_path'], '_test.sumocfg')  # 중간 파일 경로 추가
-        test(flags, configs, sumoConfig)
+            running_path, '_test.sumocfg')  # 중간 파일 경로 추가
+        test(flags, configs, sumoBinary, sumoConfig)
 
     else:  # simulate
         sumoConfig = os.path.join(
-            configs['current_path'], '_simulate.sumocfg')  # 중간 파일 경로 추가
-        simulate(flags, configs, sumoConfig)
+            running_path, '_simulate.sumocfg')  # 중간 파일 경로 추가
+        simulate(flags, configs, sumoBinary, sumoConfig)
 
 
 if __name__ == '__main__':
