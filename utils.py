e50749dfc9371ce58d24e656a49cbf4946b198d2
Yunsik-Cho <yunsik96@naver.com>
2021-05-20 01:08:30 +0900

    action mean, val

--- utils.py
+++ utils.py
@@ -11,14 +11,22 @@ def update_tensorBoard(writer, agent, env, epoch, configs,act_list):
     writer.add_scalar('episode/reward', env.reward.sum(),
                       configs['EXP_CONFIGS']['max_steps'] * epoch)
     i = 0
+    dqn = list()
+    ddpg = list()
     while i < len(act_list):
-            writer.add_histogram('action/dqn', act_list[i][1], epoch)
-            writer.add_histogram('action/ddpg', act_list[i][0], epoch)
+            #writer.add_histogram('action/dqn',epoch,act_list[i][1])
+            #writer.add_histogram('action/ddpg', epoch, act_list[i][0])
+            dqn.append(act_list[i][1])
+            ddpg.append(act_list[i][0])
             i += 1
-            
-    
-    env.reward = 0
+    dqn_tensor = torch.tensor(dqn)        
+    ddpg_tensor = torch.tensor(ddpg)
 
+    writer.add_scalar('action/dqn_mean', torch.mean(dqn_tensor), epoch)
+    writer.add_scalar('action/dqn_var', torch.var(dqn_tensor), epoch)
+    writer.add_scalar('action/ddpg_mean', torch.mean(ddpg_tensor), epoch)
+    writer.add_scalar('action/ddpg_var', torch.var(ddpg_tensor), epoch)
+    env.reward = 0    
 
 def save_params(file_path, time_data, configs):
     with open(os.path.join(file_path, 'training_data', '{}.json'.format(time_data)), 'w') as fp:
@@ -30,34 +38,7 @@ def load_params(file_path, file_name):
         configs = json.load(fp)
     return configs
 
-def show_actions(writer, action, epoch,step,act_list):
-    # if len(action) :
-    #     while num_agent > 0:
-    #         a = np.array(action)
-    #         writer.add_scalar('step/direction', a[num_agent-1][1],step)
-    #         writer.add_scalar('step/accel', a[num_agent-1][0],step)
-    #         num_agent -1
-    #         if step == 3600:
-    #             break
-    # if len(action) :
-    #     i = 0
-    #     while i < len(action):
-    #         if action[i][1] == 0:
-    #             act_list[0] += 1
-    #         elif action[i][1] == 1:
-    #             act_list[1] += 1
-    #         else:
-    #             act_list[2] += 1
-    #         i += 1 
-    #         if step == 3600:
-    #             break
-    # if step ==3600:
-    #     writer.add_scalar('action/0',act_list[0], epoch)
-    #     writer.add_scalar('action/1',act_list[1], epoch )
-    #     writer.add_scalar('action/else',act_list[2], epoch )
-    # if len(action):
-    #     act = torch.Tensor(action)
-    #     writer.add_histogram('actions', act, step)
+def show_actions(writer, action, epoch,step,act_list):    
     i = 0
     while i < len(action):
         act_list.append(action[i])
